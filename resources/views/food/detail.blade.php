e159d8a28df0c8adbcb6811e5b9805192b5fcad4
git <guzhongzhi@qq.com>
2017-05-23 15:57:53 +0800

    'index'

--- resources/views/food/detail.blade.php
+++ resources/views/food/detail.blade.php
@@ -20,7 +20,6 @@
     @parent
     <link rel="stylesheet" href="/skin/food/css/comm_v7.css">
     <link rel="stylesheet" href="/skin/food/css/food_detail.css">
-
     <script src="/skin/food/js/food.js"></script>
     <div class="sp"></div>
     <div class="container">
@@ -42,24 +41,12 @@
                    
                     <span class="fr">
                        
-                        <!--
-                        <a id="TitlePDF" class="link_pdf  a_popup_login " href="javascript:;" title="下载PDF">
-                            <i></i>下载PDF
-                        </a>
-                        
-                        <a id="TitleFavourite" href="javascript:;" class="link_collect  a_popup_login " data-favouriteid="3442551" data-favouritecategory="1" title="一键收藏">
-                            <i></i>收藏
-                        </a> <a data-shareid="3442551" data-sharecategory="0" data-share-pic="https://youimg1.c-ctrip.com/target/10070g0000007t0uw7F31.jpg" data-share-url="" data-share-title="我刚刚在@携程攻略社区 发现一篇#携程精彩游记# 『北石：江西上饶灵山告诉我，原来石头也可以一柱擎天！』很有用，仿佛跟着作者去旅行，请戳>>>" class="link_share" href="javascript:;" title="一键分享" rel="nofollow">
-                            <i></i>分享
-                        </a>
-                        <a class="link_comment " href="javascript:;" data-referencecategory="0" title="我要评论" rel="nofollow"><i></i>评论<span>1</span></a>
-                        !-->
                         
                     </span>
                 </div>
                 <div class="ctd_content">
                     <div class="ctd_content_controls cf">
-                        <div class="side_main">
+                        <div class="side_main" style="width:100%">
                             <div class="produce_info">
                                 <dl>
                                     <dt><a><img src="{{$food->getPic()}}" alt="{{$food->title}}" border="0" width="210" height="140"></a></dt>
